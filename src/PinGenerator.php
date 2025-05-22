2d176298ff1ca8a329ebb7bad5952aa1cc2fc8d6
Prashant Shinde <pashinde.7@gmail.com>
2025-05-22 12:47:14 +0530

    Initial commit: Resolved an issue in index.php

---
+++ src/PinGenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Xpl\Task;
+
+class PinGenerator
+{
+    
+    public function generate(int $totalNumberOfPin = 4, int $length = 4): array
+    {
+
+            return ['4747745',77755];
+    }
+}
